09428c2c4a3c17b7ecb0d06ae157af9d79828f7b
Said-jaouad <jaouad_sd@hotmail.com>
2024-04-07 15:04:17 -0700

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,12 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
-if ! dpkg -l | grep -q nginx; then
-	    apt-get update
-	        apt-get -y install nginx
-fi
+# sets up the web servers for the deployment of web_static
 
-# Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file
-echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" > /data/web_static/releases/test/index.html
-
-# Create or recreate the symbolic link
-if [ -L /data/web_static/current ]; then
-	    rm -f /data/web_static/current
-fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to the ubuntu user and group recursively
-chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-alias_line="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-sed -i "/server_name _;/a $alias_line" $config_file
-
-# Restart Nginx
-service nginx restart
-
-exit 0
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
